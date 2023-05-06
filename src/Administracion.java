ba1527807b78d673806b108e615ff5216a111b96
Matias Bernal <matrekb@gmail.com>
2023-05-06 19:07:08 -0300

    cdasdaas

---
+++ src/Administracion.java
@@ -0,0 +1,10 @@
+public class Administracion {
+
+    private int id;
+    private Cliente cliente;
+
+    public void llamarCliente(Cliente cliente, Materiales materiales, String numeroCelular) {
+        // Código para llamar al cliente y ofrecerle los materiales usando el número de celular
+        // ...
+    }
+}
