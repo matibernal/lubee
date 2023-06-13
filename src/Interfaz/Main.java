c700d68e5d3ec18e2a126b11508c6a05c25adf04
Keitty <116025068+Kei-Degregorio-Davinci@users.noreply.github.com>
2023-06-13 00:36:44 -0300

    Revert "main"
    This reverts commit bc9a52def96ec79efd26c550b291fa6aeefdd9b6.

--- src/Interfaz/Main.java
+++ src/Interfaz/Main.java
@@ -108,12 +108,7 @@ public class Main {
                                 String[] opci = new String[]{"Agregar trabajador", "Mostrar trabajador", "Eliminar trabajador", "Editar trabajador", "Salir"};
                                 String opcio;
                                 do {
-                                    opcio = (String)JOptionPane.showInputDialog(null,
-                                            "Bienvenido al Menu de RRHH, que desea realizar?",
-                                            "",
-                                            null,
-                                            opci,
-                                            opci[0]);
+                                    opcio = (String) JOptionPane.showInputDialog(null, "Bienvenido al Menu de RRHH, que desea realizar?", "", null, opci, opci[0]);
 
                                     switch (op) {
 
